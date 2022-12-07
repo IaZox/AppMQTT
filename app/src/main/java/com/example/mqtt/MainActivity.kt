9b7c9ce7ddf4f0334d54eda807cce1359e702913
SkyKos <skykosiv@gmail.com>
2022-12-07 11:21:24 +0100

    first commit

---
+++ app/src/main/java/com/example/mqtt/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.mqtt
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import org.eclipse.paho.client.mqttv3.*
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+
+private lateinit var instance: MainActivity
+var broker = "tcp://test.mosquitto.org:1883"
+// TLS/SSL
+// String broker = "ssl://broker.emqx.io:8883";
+var username = ""
+
+var password = ""
+var clientid = "Client"
+val content = "Louis le bg";
+val client = MqttClient(broker, clientid, MemoryPersistence())
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        instance = this;
+        val encodedPayload : ByteArray
+        try {
+            val options = MqttConnectOptions()
+            options.userName = username
+            options.password = password.toCharArray()
+            client.connect(options)
+            encodedPayload = content.toByteArray(charset("UTF-8"))
+            val message = MqttMessage(encodedPayload)
+            message.qos = 2
+            message.isRetained = false
+            client.publish("zoubidac", message)
+            client.subscribe("zoubidac", 1);
+            client.subscribe("humidity", 1);
+            Log.d("MQTT", "MEssage Publish")
+            receiveMessages();
+        } catch (e: Exception) {
+            // Give Callback on error here
+        } catch (e: MqttException) {
+            // Give Callback on error here
+        }
+    }
+}
+
+fun receiveMessages() {
+    client.setCallback(object : MqttCallback {
+        override fun connectionLost(cause: Throwable) {
+            //connectionStatus = false
+            // Give your callback on failure here
+        }
+        override fun messageArrived(topic: String, message: MqttMessage) {
+            try {
+                val data = String(message.payload, charset("UTF-8"))
+                if (topic == "humidity") {
+                    Log.d("Il fait ", data + " °C");
+                } else {
+                    Log.d(topic, data);
+                }
+                // Give your callback on message received here
+            } catch (e: Exception) {
+                // Give your callback on error here
+            }
+        }
+        override fun deliveryComplete(token: IMqttDeliveryToken) {
+            // Acknowledgement on delivery complete
+        }
+    })
+}
